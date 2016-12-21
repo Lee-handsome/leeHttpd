beca9d24bbd587f8408056708a3e94344dfe59af
lijun <1317569173@qq.com>
2016-12-21 23:50:03 +0800

    v0.2

--- httpServer.cpp
+++ httpServer.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -11,30 +12,38 @@
 #include <iostream>
 using namespace std;
 
-#define PORT 9999
+#define PORT 8888
 #define MAX_BUF 1024*8
 
 void* accept_request(void* clientfd);
 void do_get(char* url,int client);
-void do_post(char* url,int client);
+void do_post(char* url,int client,char* content);
 void send_staticfile(int client, char* path);
-void startup_cgi();
+void startup_cgi(int client,char* method,char* path,char* param,char* content);
 void response404(int client);
 void send_header(int client);
 
 void* accept_request(void* clientfd)
 {
-
 	int client=(int)*((int*)clientfd);
-	cout<<"accept:"<<client<<endl;
-	char buf[MAX_BUF];
+	//cout<<"accept:"<<client<<endl;
+	char request[MAX_BUF];
+	char* content;
+	char* line;
 	char* method;
 	char* url;
 
-	recv(client,buf,sizeof(buf)-1,0);
+	recv(client,request,sizeof(request),0);
 	cout<<endl;
-	cout<<"request:"<<buf<<endl;
-	method = strtok(buf," ");
+	cout<<"accept:"<<client<<"request:"<<request<<endl;
+	char* ptr= strtok(request,"\r\n");
+	line = ptr;
+	while(ptr!=NULL){
+		content=ptr;
+		ptr=strtok(NULL,"\r\n");
+	}
+	
+	method=strtok(line," ");
 	cout<<"method:"<<method<<endl;
 	url = strtok(NULL," ");
 	cout<<endl;
@@ -44,7 +53,8 @@ void* accept_request(void* clientfd)
 		do_get(url,client);
 	}
 	else if(strcmp(method,"POST")==0){
-		do_post(url,client);
+		cout<<"content:"<<content<<endl;
+		do_post(url,client,content);
 	}
 	else response404(client);
 	pthread_exit(NULL);
@@ -54,6 +64,7 @@ void do_get(char* url,int client)
 {
 	char* ptr=url;
 	char path[512];
+	char* param;
 	bool cgi=0;
 	int i=0;
 	while(*ptr!='\0') {
@@ -65,12 +76,16 @@ void do_get(char* url,int client)
 		ptr++;
 	}
 	path[i]='\0';
+	cout<<"path"<<path<<endl;
+	param = ptr;
+	//cout<<"param:"<<param<<endl;
+
 	if(path[strlen(path)-1]=='/'){
 		strcat(path,"/index.html");
 	}
 
 	if(cgi){
-		startup_cgi();
+		startup_cgi(client,"GET",path,param,NULL);
 	}
 	else{
 		send_staticfile(client,path);
@@ -78,8 +93,12 @@ void do_get(char* url,int client)
 	close(client);
 }
 
-void do_post(char* url,int client)
+void do_post(char* url,int client,char* content)
 {
+	char path[512];
+	startup_cgi(client,"POST",url,NULL,content);
+	cerr<<"post close"<<endl;
+	close(client);
 
 }
 void send_staticfile(int client, char* path)
@@ -87,7 +106,7 @@ void send_staticfile(int client, char* path)
 	FILE* fp;
 	//strcat(".",path);
 	char pathname[512];
-	sprintf(pathname, "www/%s", path);
+	sprintf(pathname, "www%s", path);
 	fp=fopen(pathname,"rb");
 	char buf[1024];
 	if(fp==NULL){
@@ -103,19 +122,104 @@ void send_staticfile(int client, char* path)
         send(client, buf, strlen(buf), 0); 
         fgets(buf, sizeof(buf), fp);
     }
-    close(client);
+    cout<<"@@@@@@@@@"<<"duwan"<<endl;
+    fclose(fp);
+    //close(client);
 }
 
-void startup_cgi()
+void startup_cgi(int client,char* method,char* path,char* param,char* content)
 {
+	int cgi_in[2];
+	int cgi_out[2];
+	pid_t pid;
+	char status[] = "HTTP/1.0 200 OK\r\n";
+	send(client,status,strlen(status),0);
 
+	if(-1==pipe(cgi_in)){
+		perror("pipe error");
+		return;
+	}
+	if(-1==pipe(cgi_out)){
+		perror("pipe error");
+		return;
+	}
+	if ((pid = fork()) < 0 ) {
+        perror("fork error");
+        return;
+    }
+    if (pid == 0)
+    {
+    	cout<<"i am cgi"<<endl;
+    	char pathname[512];
+		sprintf(pathname,"./www%s",path);
+		cout<<"pathname:"<<pathname<<endl;
+    	//cout<<"path"<<path<<endl;
+        char method_env[512];
+        char query_env[512];
+        char length_env[512];
+
+        dup2(cgi_out[1], 1);
+        dup2(cgi_in[0], 0);
+
+        close(cgi_out[0]);
+        close(cgi_in[1]);
+
+        sprintf(method_env, "REQUEST_METHOD=%s", method);
+        putenv(method_env);
+
+        if (strcmp(method, "GET") == 0) {
+            sprintf(query_env, "QUERY_STRING=%s", param);
+            putenv(query_env);
+        }
+        else 
+        {
+        	int content_length = strlen(content);
+   			sprintf(length_env, "CONTENT_LENGTH=%d", content_length);
+            putenv(length_env);
+        }
+        //
+        cerr<<pathname<<endl;
+        execl(pathname, pathname, NULL);
+        perror("exec error");
+        response404(client);
+        exit(0);
+    } 
+    else 
+    {    /* parent */
+        close(cgi_out[1]);
+        close(cgi_in[0]);
+
+        char buf[1024];
+        //char content[1024];
+        if (strcmp(method, "POST") == 0)
+        {
+        	//cerr<<"buf:"<<buf<<endl;
+        	write(cgi_in[1],content,strlen(content)+1);
+        }
+        
+            //for (i = 0; i < content_length; i++) {
+            //    recv(client, content, 1, 0);
+            //    write(cgi_input[1], content, 1);
+            //}
+
+        //send_header(client);
+        while (read(cgi_out[0], buf, sizeof(buf)) > 0){
+        	cerr<<"buf:"<<buf<<endl;
+            send(client, buf, sizeof(buf), 0);
+        }
+
+        close(cgi_out[0]);
+        close(cgi_in[1]);
+        waitpid(pid, NULL, 0);
+    }
 }
+
 void response404(int client)
 {
 	send_header(client);
 	char body[] = "<html><body>404 Not Found from leeHttpd</body></html>";
 	send(client,body,strlen(body),0);
-	//close(client);
+	close(client);
 }
 
 void send_header(int client)
@@ -140,6 +244,12 @@ int main()
 		perror("socket error");
 		exit(1);
 	}
+	int opt = 1;
+	if(-1 == setsockopt(server_sock,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt)))
+	{
+		perror("setsockopt error");
+		exit(1);
+	}
 
 	//init server_addr
 	memset(&server_addr,0,sizeof(server_addr));
@@ -151,7 +261,7 @@ int main()
 		exit(1);
 	}
 
-	if(-1 == listen(server_sock,5)){
+	if(-1 == listen(server_sock,1024)){
 		perror("listen error");
 		exit(1);
 	}
---
+++ www/mycolor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '</head>'
+print '<body>'
+print '<h2>Hello Word! 我是一个CGI程序</h2>'
+print '</body>'
+print '</html>'
